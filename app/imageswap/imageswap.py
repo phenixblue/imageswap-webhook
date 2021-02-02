673a8f063959a96aa442e8e09be16cd5be5c9afb
Joe Searcy <joe@twr.io>
2021-02-02 02:42:41 -0500

    Lint python post changes

--- app/imageswap/imageswap.py
+++ app/imageswap/imageswap.py
@@ -84,9 +84,13 @@ def mutate():
     app.logger.debug(json.dumps(request.json))
 
     # Skip patching if disable label is found and set to "disable"
-    if "labels" in workload_metadata and imageswap_disable_label in workload_metadata["labels"] and workload_metadata["labels"][imageswap_disable_label] == "disabled":
-        
-        app.logger.info(f"Disable label \"{imageswap_disable_label}=disabled\" detected, skipping image swap.")
+    if (
+        "labels" in workload_metadata
+        and imageswap_disable_label in workload_metadata["labels"]
+        and workload_metadata["labels"][imageswap_disable_label] == "disabled"
+    ):
+
+        app.logger.info(f'Disable label "{imageswap_disable_label}=disabled" detected, skipping image swap.')
         needs_patch = False
 
     else:
