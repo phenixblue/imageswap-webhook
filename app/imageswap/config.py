14488baf025e6a79c517a47bbaccd1c2bde6b6c6
Joe Searcy <joe@twr.io>
2021-06-03 02:51:40 -0400

    Cleanup comments

--- app/imageswap/config.py
+++ app/imageswap/config.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-## Some random text to initiate a change
-
 # Gunicorn config
 bind = ":5000"
 workers = 2
